64238a88b3a3b3268364f3bad039fc5d91d13039
chillehh <jacobrling@gmail.com>
2021-11-17 14:34:02 +1030

    Print correct output
    - Printing output to command line in the  YAML format specified by original task.

--- main.py
+++ main.py
@@ -10,30 +10,25 @@ with open('data.csv') as csvData:
     # Set the data to a list for sorting
     data = list(reader)
 
-print(data) # Testing purposes
-
-# Originally using a bool to set to true when the first object in the list was deleted but realised I could just delete
-# the first object before iterating over the list
-deleteTop = False
-
 # Delete the first lot of data as its just the names of the columns
 del data[0]
 
+# Convert the division and points columns to ints for sorting
 for i in range(len(data)):
-    print(data[i]) # Testing purposes
     div = data[i]
     div[division] = int(div[division])
     point = data[i]
     point[points] = int(point[points])
-        #data[i[division]] = data[int(i[division])]
-        #i[points] = i[int(points)]
-    print('changed to Int') # Testing purposes
-    #elif i == 0 and not deleteTop:
-     #   print('deleting..')
-      #  del data[i]
-       # deleteTop = True
-        #continue
+
 
 # Sort the data by division from lowest to highest and then by points from highest to lowest
 data.sort(key=lambda x: (x[division], -x[points]))
-print(data) # Show the sorted data for output
+
+# Print initial 'records: '
+print('records: ')
+
+# Loop over the top 3 results and print accordingly
+for i in range(len(data)):
+    if i < 3:
+        result = data[i]
+        print('- name: ' + result[0] + ' ' + result[1] + '\ndetails: In division ' + str(result[division]) + ' from ' + result[2] + ' performing ' + result[5])
